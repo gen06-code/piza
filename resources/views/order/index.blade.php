091c17b417ef9829bfe797fd78a7691a93928eae
gen06 <gen06@pbk.edu.hk>
2025-10-17 14:18:07 +0800

    section 25 end, design table OK

--- resources/views/order/index.blade.php
+++ resources/views/order/index.blade.php
@@ -1,23 +1,57 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <div class="row justify-content-center">
-        <div class="col-md-8">
-            <div class="card">
-                <div class="card-header">order</div>
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-12">
+                <nav style="--bs-breadcrumb-divider: '>';" aria-label="breadcrumb">
+                    <ol class="breadcrumb">
+                        <li class="breadcrumb-item"><a href="#">Home</a></li>
+                        <li class="breadcrumb-item active" aria-current="page">Orders</li>
+                    </ol>
+                </nav>
 
-                <div class="card-body">
-                    @if (session('status'))
-                        <div class="alert alert-success" role="alert">
-                            {{ session('status') }}
-                        </div>
-                    @endif
+                <div class="card">
+                    <div class="card-header">order</div>
 
-                    {{ __('You are logged in!') }}
+                    <div class="card-body">
+                        <table class="table">
+                            <thead>
+                                <tr>
+                                    <th scope="col">User</th>
+                                    <th scope="col">Phone/Email</th>
+                                    <th scope="col">Date/Time</th>
+                                    <th scope="col">Pizza</th>
+                                    <th scope="col">Small pizza</th>
+                                    <th scope="col">Medium pizza</th>
+                                    <th scope="col">Large pizza</th>
+                                    <th scope="col">status</th>
+                                    <th scope="col">Accept</th>
+                                    <th scope="col">Reject</th>
+                                    <th scope="col">Completed</th>
+
+                                </tr>
+                            </thead>
+                            <tbody>
+                                <tr>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td>@mdo</td>
+                                    <td>Mark</td>
+                                    <td>Otto</td>
+                                    <td><button class="btn btn-primary">Accept</button></td>
+                                    <td><button class="btn btn-primary">Reject</button></td>
+                                    <td><button class="btn btn-primary">Completed</button></td>
+                                </tr>
+                                
+                            </tbody>
+                        </table>
+                    </div>
                 </div>
             </div>
         </div>
     </div>
-</div>
 @endsection
