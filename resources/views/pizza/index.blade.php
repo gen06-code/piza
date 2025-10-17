f3a8c3700f589de22ea053bb35cc22980f580e4c
gen06 <gen06@pbk.edu.hk>
2025-10-17 10:38:49 +0800

    Created userOrderControler and order Main page, section 23 User Order

--- resources/views/pizza/index.blade.php
+++ resources/views/pizza/index.blade.php
@@ -3,7 +3,33 @@
 @section('content')
     <div class="container">
         <div class="row justify-content-center">
-            <div class="col-md-8">
+            <div class="col-md-3">
+            <div class="card">
+                <div class="card-header">Menu</div>
+
+                <div class="card-body">
+                    <ul class="list-group">
+                        <a href="{{route('pizza.index')}}" class="list-group-item list-group-item-action">View</a>
+                        <a href="{{route('pizza.create')}}" class="list-group-item list-group-item-action">Create</a>
+                        <a href="{{route('user.order')}}" class="list-group-item list-group-item-action">User order</a>
+                    </ul>
+                </div>
+            </div>
+            @if (count($errors)>0)
+            <div class="card mt-5">
+                <div class="card-body">
+                    
+                    <div class="alert alert-danger">
+                        @foreach ($errors->all() as $error)
+                        <p>{{ $error }}</p>
+                        @endforeach
+                    </div>
+                    
+                </div>
+            </div>
+            @endif
+        </div>
+            <div class="col-md-9">
                 <div class="card">
                     <div class="card-header">All Pizza
                         <a href="{{route('pizza.create')}}">
