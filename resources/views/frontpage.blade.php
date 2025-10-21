83f7cc64a35526328cbc966009a348cbf011b916
gen06 <gen06@pbk.edu.hk>
2025-10-21 14:53:35 +0800

    section 30 end, list all pizza

---
+++ resources/views/frontpage.blade.php
@@ -0,0 +1,65 @@
+@extends('layouts.app')
+
+@section('content')
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="col-md-4">
+                <div class="card">
+                    <div class="card-header">Menu</div>
+
+                    <div class="card-body">
+                        <ul class="list-group">
+                            <a href="" class="list-group-item list-group-item-action">Category1</a>
+                            <a href="" class="list-group-item list-group-item-action">Category2</a>
+                            <a href="" class="list-group-item list-group-item-action">Category3</a>
+                            <a href="" class="list-group-item list-group-item-action">Category4</a>
+                            <a href="" class="list-group-item list-group-item-action">Category5</a>
+                        </ul>
+
+                    </div>
+                </div>
+            </div>
+            <div class="col-md-8">
+                <div class="card">
+                    <div class="card-header">Pizza</div>
+
+                    <div class="card-body">
+                        <div class="row">
+                            @forelse ($pizzas as $pizza)
+                               <div class="col-md-4 mt-2 text-center" style="border:1px solid #ccc;">
+                                <img src="{{Storage::url($pizza->image)}}" class="img-thumbnail" style="width:100%;"/>
+                                <p>{{$pizza->name}}</p>
+                                <p>{{$pizza->description}}</p>
+                                <a href="">
+                                    <button class="btn btn-danger mb-1">Order Now</button>
+                                </a>
+                            </div> 
+                            @empty
+                            <p>no pizza to show</p>
+                                
+                            @endforelse
+                            
+                        </div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    </div>
+
+    <style>
+        
+        a.list-group-item{
+            font-size:18px;
+        }
+        a.list-group-item:hover{
+            color: #FFF;
+            background-color: red;
+        }
+        .card-header{
+            background-color: #F00;
+            color: #FFF;
+            font-size:20px;
+        }
+    </style>
+@endsection
--- routes/web.php
+++ routes/web.php
@@ -13,13 +13,13 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
-Route::get('/', function () {
-    return view('welcome');
-});
+
 
 Auth::routes();
 
 Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
+Route::get('/', [App\Http\Controllers\FrontendController::class, 'index'])->name('frontpage');
 
 Route::group(['middleware'=>'auth','admin'],function(){
     Route::get('/pizza', [App\Http\Controllers\PizzaController::class, 'index'])->name('pizza.index');
